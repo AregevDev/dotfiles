6b2e96f2be91108537c5a441180c2e7b60893273
AregevDev <aregevdev@gmail.com>
2024-08-02 18:47:32 +0300

    Fix nvim-web-devicons plugin

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
     tag = '0.1.8',
     dependencies = {
         { 'nvim-telescope/telescope-fzf-native.nvim', build = make }, 
-        'nvim-tree/nvim-web-davicons'
+        'nvim-tree/nvim-web-devicons'
     },
     config = function()
         local telescope = require('telescope')
