958eaa5df38d1179c246d6c874feefc547976511
AregevDev <aregevdev@gmail.com>
2024-08-02 18:08:18 +0300

    Some more keymaps

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -7,5 +7,10 @@ keymap.set('n', '<leader>tq', '<cmd>tabclose<CR>', {});
 keymap.set('n', '<A-e>', '<cmd>tabnext<CR>', {});
 keymap.set('n', '<A-q>', '<cmd>tabprevious<CR>', {});
 
--- Telescope
-keymap.set('n', '<leader>ff', telescope.find_files, {}) 
+-- telescope
+keymap.set('n', '<leader>ff', telescope.find_files, {})
+keymap.set('n', '<leader>fl', telescope.live_grep, {})
+
+-- which-key
+keymap.set('n', '<leader>w', '<cmd>WhichKey<CR>', {})
+
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,18 @@
 return {
     'nvim-telescope/telescope.nvim', 
-    tag = '0.1.8'
+    tag = '0.1.8',
+    dependencies = {
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = make }, 
+        'nvim-tree/nvim-web-davicons'
+    },
+    config = function()
+        local telescope = require('telescope')
+        local actions = require('telescope.actions')
+
+        telescope.setup({
+            defaults = {
+                path_display = 'smart'
+            },
+        })
+    end
 }
